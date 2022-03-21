211b207f0db09c9ad74a30eaf4dc1bd35dc33a23
Pravinkhot7117 <101989135+Pravinkhot7117@users.noreply.github.com>
2022-03-21 19:43:52 +0530

    Add files via upload

---
+++ Thinkbridge_ShopBridge/AppCode/Inventry.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+public class Inventry
+{
+   //static string cs = ConfigurationManager.ConnectionStrings["Constring"].ConnectionString;
+   SqlConnection con=new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+
+   DataTable dt=new DataTable();
+   //SqlTransaction Trans = default(SqlTransaction);
+   //SqlCommand cmd = new SqlCommand();
+
+   #region Save
+   public string Save(int Id, string Name, string Descreption, decimal Price, int Quantity, string OperationType)
+   {
+      SqlTransaction Trans = default(SqlTransaction);
+      SqlCommand cmd = new SqlCommand();
+      string Status = null;
+      con.Open();
+      //Trans = con.BeginTransaction();
+      try
+      {
+         cmd.Connection = con;
+         cmd.CommandType = CommandType.StoredProcedure;
+         cmd.CommandText = "SP_InventryMaster";
+         cmd.CommandTimeout = 0;
+         //if (OperationType == "Update")
+         //{
+            cmd.Parameters.Add("@ItemID", SqlDbType.Int).Value = Id;
+         //}
+         //else
+         //{
+         //   cmd.Parameters.Add("@ItemID", SqlDbType.Int).Value = DBNull.Value;
+         //}
+         cmd.Parameters.Add("@ItemName", SqlDbType.NVarChar).Value = Name;
+         cmd.Parameters.Add("@Description", SqlDbType.NVarChar).Value = Descreption;
+         cmd.Parameters.Add("@ItemPrice", SqlDbType.Decimal).Value = Price;
+         cmd.Parameters.Add("@ItemQuantity", SqlDbType.Int).Value = Quantity;
+         cmd.Parameters.Add("@OperationType", SqlDbType.NVarChar).Value = OperationType;
+         cmd.ExecuteNonQuery();
+         //Trans.Commit();
+         Status = "Success";
+      }
+      catch (Exception ex)
+      {
+         ex.Message.ToString();
+         Status = "Failed to "+ OperationType;
+         Trans.Rollback();
+      }
+      finally
+      {
+         con.Close();
+      }
+      return Status;
+   }
+   #endregion
+
+   public DataTable SearchItem(string ItemName, decimal ItemPrice)
+   {
+      DataTable dt = new DataTable();
+      try
+      {
+         SqlDataAdapter adapt = new SqlDataAdapter();
+         SqlCommand cmd = new SqlCommand();
+         cmd.Connection = con;
+         cmd.CommandType = CommandType.StoredProcedure;
+         cmd.CommandText = "SP_GET_InventryDetails";
+         cmd.CommandTimeout = 0;
+         if (ItemName != "" && ItemName != null)
+         {
+            cmd.Parameters.Add("@ItemName", SqlDbType.NVarChar).Value = ItemName;
+         }
+         else
+         {
+            cmd.Parameters.Add("@ItemName", SqlDbType.NVarChar).Value = DBNull.Value;
+         }
+         if (ItemPrice != 0)
+         {
+            cmd.Parameters.Add("@ItemPrice", SqlDbType.Decimal).Value = ItemPrice;
+         }
+         else
+         {
+            cmd.Parameters.Add("@ItemPrice", SqlDbType.NVarChar).Value = DBNull.Value;
+         }
+         adapt.SelectCommand = cmd;
+         adapt.Fill(dt);
+      }
+      catch (Exception ex)
+      {
+         ex.Message.ToString();
+      }
+      return dt;
+   }
+}
---
+++ Thinkbridge_ShopBridge/JS/PageValidate.js
@@ -0,0 +1,52 @@
+﻿function validate() {
+    var name = document.getElementById("txt_ItemName");
+    var ItemPrice = document.getElementById("txt_ItemPrice");
+    var Quantity = document.getElementById("txt_Quantity");
+    var status = true;
+
+    if (name.value == '') {
+        alert("Please enter item name",name.focus());
+        status = false;
+    } else if (ItemPrice.value <= 0) {
+        alert("Please enter item price", ItemPrice.focus());
+        status = false;
+    } else if (Quantity.value <= 0) {
+        alert("Please enter item price", Quantity.focus());
+        status = false;
+    }
+    return status;
+}
+
+function Clear() {
+    var name = document.getElementById("txt_ItemName");
+    var ItemPrice = document.getElementById("txt_ItemPrice");
+    var Desciption = document.getElementById("txt_ItemDescription");
+    var Quantity = document.getElementById("txt_Quantity");
+
+    name.value = '';
+    ItemPrice.value = '';
+    Desciption.value = '';
+    Quantity.value = '';
+}
+
+function confirmAction() {
+    let confirmAction = confirm("Are you sure delete this reord?");
+    if (confirmAction) {
+        alert("Action successfully executed");
+    }
+}
+
+function Confirm() {
+    var confirm_value = document.createElement("INPUT");
+    confirm_value.type = "hidden";
+    confirm_value.name = "confirm_value";
+    confirm_value.value = "";
+    if (confirm("Are you sure delete this reord?")) {
+        //confirm_value.value = "Yes";
+        hdn_ConfirmValue.value = "Yes";
+    } else {
+        hdn_ConfirmValue.value = "No";
+    }
+    //document.forms[0].appendChild(confirm_value);
+}
+
